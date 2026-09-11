fd883863fcd875f0ca51cd9b0a2895869276efc3
Marquis Davis <102924796+Marquis-Davis@users.noreply.github.com>
2026-09-11 13:26:47 -0400

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,6 @@
-#Ready
+Get-ComputerInfo |
+Select WindowsProductName, WindowsVersion, OsBuildNumber, OsArchitecture
+
+Get-WindowsOptionalFeature -Online |
+Where-Object FeatureName -match 'NetFx' |
+Select FeatureName, State
