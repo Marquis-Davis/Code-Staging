1d0dcaf313a5a2fcdfa8c75773803b0ad9ffd5ff
Marquis Davis <102924796+Marquis-Davis@users.noreply.github.com>
2026-09-18 16:25:43 -0400

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,17 +1,5 @@
-# Find the offline Windows installation
-$WindowsDrive = Get-PSDrive -PSProvider FileSystem |
-    Where-Object {
-        $_.Root -ne $env:SystemDrive -and
-        (Test-Path (Join-Path $_.Root 'Windows\System32\Config\SOFTWARE'))
-    } |
-    Select-Object -First 1
+$OSBuildPath = "Registry::HKEY_LOCAL_MACHINE\$HiveName\Microsoft\Windows NT\CurrentVersion"
 
-if (-not $WindowsDrive) {
-    throw 'Windows volume not found.'
-}
-
-$WindowsRoot = Join-Path $WindowsDrive.Root 'Windows'
-
-# Set these for the Capture Operating System Image step
-$ts.Value('OSDTargetSystemDrive') = $WindowsDrive.Root.TrimEnd('\')
-$ts.Value('OSDTargetSystemRoot') = $WindowsRoot
+$OSBuild = Get-ItemPropertyValue `
+    -Path $OSBuildPath `
+    -Name 'CurrentBuild'
